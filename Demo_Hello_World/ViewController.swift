ffffb105d03764bf8a0e97c031e92f133d4bfcf0
Aarthi <aarthi@Aarthis-MacBook-Pro.local>
2022-06-08 22:47:25 +0530

    Initial Project set-up

---
+++ Demo_Hello_World/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Demo_Hello_World
+//
+//  Created by Aarthi on 08/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
