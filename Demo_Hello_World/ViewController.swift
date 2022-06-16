bb748ba8ddddb8ca84ac972f43fabd5cd20eb370
Aarthi <aarthi@Aarthis-MacBook-Pro.local>
2022-06-16 16:16:29 +0530

    UAT Commit 1

--- Demo_Hello_World/ViewController.swift
+++ Demo_Hello_World/ViewController.swift
@@ -13,10 +13,12 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        
+        initProcess()
         print("Welcome!")
     }
-
+    func initProcess() {
+        print("Push to UAT - 1")
+    }
 
 }
 
